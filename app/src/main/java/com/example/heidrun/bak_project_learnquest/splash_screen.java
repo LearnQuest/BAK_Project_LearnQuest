8f3bd3d0204e466cac756e81341f5e01a49b85cd
seifenblase98 <fraka98@gmx.net>
2018-12-17 11:35:04 +0100

    splash screen erstellung

---
+++ app/src/main/java/com/example/heidrun/bak_project_learnquest/splash_screen.java
@@ -0,0 +1,13 @@
+package com.example.heidrun.bak_project_learnquest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splash_screen extends AppCompatActivity {
+private static int splash_time_out= 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
