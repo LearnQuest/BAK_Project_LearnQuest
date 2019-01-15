3b0cf8361dcf29cf054c1304b97bf7bfcef6bced
seifenblase98 <fraka98@gmx.net>
2019-01-15 22:30:25 +0100

    SplashScreen wurde mit lottiefile ausgestattet und ein Dialogfenster für die Wahl eines Characters wurde hinzugefügt

--- app/src/main/java/com/example/heidrun/bak_project_learnquest/MainActivity.java
+++ app/src/main/java/com/example/heidrun/bak_project_learnquest/MainActivity.java
@@ -15,6 +15,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -88,5 +89,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
     }
+    public void onClickOpenDialogue(View view){
+        final Dialog f = new Dialog(MainActivity.this);
+        f.setTitle("Choose your character");
+        f.setContentView(R.layout.activity_choose_character);
+        f.show();
+       }
 }
 
---
+++ app/src/main/java/com/example/heidrun/bak_project_learnquest/chooseCharacter.java
@@ -0,0 +1,14 @@
+package com.example.heidrun.bak_project_learnquest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class chooseCharacter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_character);
+    }
+
+}
