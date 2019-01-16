c3c190bea615256ac9753c9e220324c284e1617b
LearnQuest <heidrun.semler@edu.fh-joanneum.at>
2019-01-16 17:35:19 +0100

    Überarbeitung Subject Fragment, Appicon Änderungen

--- app/src/main/java/com/example/heidrun/bak_project_learnquest/MainActivity.java
+++ app/src/main/java/com/example/heidrun/bak_project_learnquest/MainActivity.java
@@ -11,14 +11,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //hi test this push function
 
-        questionFragment frg = new questionFragment();
+        //questionFragment frg = new questionFragment();
        // getSupportFragmentManager().beginTransaction().add(R.id.container, frg).commit();
 
-        trophiesFragment troph = new trophiesFragment();
+        //trophiesFragment troph = new trophiesFragment();
         //getSupportFragmentManager().beginTransaction().add(R.id.container, troph).commit();
 
-        subjectFragment subj = new subjectFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.container, subj).commit();
+       // subjectFragment subj = new subjectFragment();
+        //getSupportFragmentManager().beginTransaction().add(R.id.container, subj).commit();
 
     }
 
